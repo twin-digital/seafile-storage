20afda49a2ab8e83d91a4863d40a206674a4b0df
Sean Kleinjung <sean@thrashplay.com>
2023-03-02 00:20:17 -0600

    Add s3 bucket and access role.

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,9 +9,9 @@ const project = new TwinDigitalCdkApp({
     authProvider: CodeArtifactAuthProvider.GITHUB_OIDC,
     roleToAssume: 'arn:aws:iam::934979133063:role/CommonCicd-RootGitHubGithubActionsACC56793-79JE121HOPUG',
   },
-  deps: ['@twin-digital/cdk-patterns', 'js-yaml', 'lodash'],
+  deps: ['@twin-digital/cdk-patterns'],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: ['@twin-digital/projen', '@types/js-yaml', '@types/lodash', 'cdk-pipelines-github'],
+  devDeps: ['@twin-digital/projen', 'cdk-pipelines-github'],
   name: 'iam-core-aws',
 })
 
